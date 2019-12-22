b5b5e80d944160085c815efa21317afa805ca651
Livia Magureanu <livia@Livias-MacBook-Pro-2.local>
2019-12-22 16:41:58 +0200

    clean-up

--- Lab Work/Lab 5/LCDDisplay/LCDDisplay.ino
+++ Lab Work/Lab 5/LCDDisplay/LCDDisplay.ino
@@ -1,16 +1,16 @@
 #include <LiquidCrystal.h>
 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+LiquidCrystal lcd(8, 9, 2, 3, 4, 5);
 
 void setup() {
   // put your setup code here, to run once:
   lcd.begin(16, 2);
-  lcd.setCursor(0, 0);
+  lcd.setCursor(0, 1);
   lcd.print("hello, world!");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  lcd.setCursor(6, 0);
-  lcd.print(millis() / 1000);
+//  lcd.setCursor(6, 0);
+//  lcd.print(millis() / 1000);
 }
